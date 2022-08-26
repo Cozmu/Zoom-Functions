f4233f60f37c8164bc32182025bcee28abfa4ee0
Cozmu <www.jorgereis@gmail.com>
2022-08-26 17:42:12 -0300

    Adicionando requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  return employees
+    .find(({ firstName, lastName }) => employeeName === firstName || employeeName === lastName);
 }
-
 module.exports = getEmployeeByName;
