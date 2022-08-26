0388322d8c457414538727c16f3a56601c26fc1c
Cozmu <www.jorgereis@gmail.com>
2022-08-26 19:16:54 -0300

    Adicionando requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,14 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some(({ managers }) => managers.includes(id));
 }
-
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return employees.filter((element) => element.managers.includes(managerId))
+    .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
 }
-
 module.exports = { isManager, getRelatedEmployees };
