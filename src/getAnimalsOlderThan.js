f0bc92229178119eb4f8b0e9c02eb845334e5d3a
Cozmu <www.jorgereis@gmail.com>
2022-08-26 16:43:54 -0300

    Adicionando requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species.some(({ name, residents }) => {
+    return name === animal && residents.every((element) => element.age >= age);
+  });
 }
-
 module.exports = getAnimalsOlderThan;
