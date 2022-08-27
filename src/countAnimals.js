7c6f2f3ccdb4c4c382bfaddd14c3360192df2989
Cozmu <www.jorgereis@gmail.com>
2022-08-26 22:08:21 -0300

    Add requisito 5 sem especificação por sexo

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((acumulador, { name, residents }) => {
+      acumulador[name] = residents.length;
+      return acumulador;
+    }, {});
+  }
+  // if (animal.sex) {
+  //   return 
+  // }
+  return species.find(({ name }) => name === animal.specie).residents.length;
 }
-
+console.log(countAnimals({ specie: 'giraffes' }));
 module.exports = countAnimals;
+// return species.find((element) => element.name === animal.specie).residents.length;
