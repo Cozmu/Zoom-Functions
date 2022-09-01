8a171cc847e043eae1cd2d795c5be1df9628c3f0
Cozmu <www.jorgereis@gmail.com>
2022-08-31 21:01:33 -0300

    Iniciando requisito 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,15 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+function getAnimalsLocal() {
+  const principal = { NE: [], NW: [], SE: [], SW: [] };
+  const chaves = Object.keys(principal);
+  return species.filter(({ location }) => chaves.includes(location)).map(({ name }) =>)
+}
+console.log(getAnimalsLocal());
+
 function getAnimalMap(options) {
-  // seu código aqui
+  // seu código aqui\
 }
 
 module.exports = getAnimalMap;
